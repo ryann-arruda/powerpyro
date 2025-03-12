2b69ea7a52b1eddd37e0f628758ecc0551fb09c6
Ryann <ryann.arruda_159@hotmail.com>
2025-03-12 17:43:22 -0300

    Adding a new field: operating_system. Additionally, the necessary methods to access this field have been implemented.

--- power_pyro/hardware_component.py
+++ power_pyro/hardware_component.py
@@ -1,16 +1,20 @@
 from abc import ABC, abstractmethod
-import os
+from os_type import OsType
 
 class HardwareComponent(ABC):
-    def __init__(self):
+    def __init__(self, operating_system: OsType):
         self.__name:str
         self.__total_energy_consumed:float
+        self.__operating_system: OsType = operating_system
     
     def get_name(self) -> str:
         return self.__name
 
     def get_total_energy_consumed(self) -> float:
         return self.__total_energy_consumed
+
+    def get_operating_system(self) -> OsType:
+        return self.__operating_system
     
     def update_energy_consumed(self, energy_consumed_per_time: float) -> None:
         self.__total_energy_consumed += energy_consumed_per_time
