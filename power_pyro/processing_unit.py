9ace4bc4431a50a17a1d479540561d5d2b6d84f2
Ryann <ryann.arruda_159@hotmail.com>
2025-03-16 20:20:16 -0300

    Creating an abstract subclass of HardwareComponent to specify a generic processing unit

---
+++ power_pyro/processing_unit.py
@@ -0,0 +1,29 @@
+from hardware_component import HardwareComponent
+from os_type import OsType
+
+from elevate import elevate
+import clr
+import os
+from abc import abstractmethod
+
+if os.name == 'nt':
+    elevate()
+
+    #clr.AddReference(r"C:\Users\Alexa\OneDrive\Área de Trabalho\LibreHardwareMonitor\LibreHardwareMonitorLib.dll")
+    clr.AddReference(r"C:\LibreHardwareMonitor\LibreHardwareMonitorLib.dll")
+    from LibreHardwareMonitor.Hardware import Computer
+
+class ProcessingUnit(HardwareComponent):
+
+    def __init__(self, operating_system: OsType):
+        super().__init__(operating_system)
+
+        if operating_system == OsType.WINDOWS:
+            self.__computer: Computer = Computer()
+    
+    def get_computer(self) -> Computer:
+        return self.__computer
+    
+    @abstractmethod
+    def __update_manufacture(self) -> None:
+        pass
