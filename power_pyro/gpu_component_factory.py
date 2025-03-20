2802383cd9d341a764b6036042f119fb6df179e8
Ryann <ryann.arruda_159@hotmail.com>
2025-03-20 17:38:29 -0300

    Addition of operating_system parameter

--- power_pyro/gpu_component_factory.py
+++ power_pyro/gpu_component_factory.py
@@ -1,4 +1,5 @@
 from hardware_component_factory import HardwareComponentFactory
+from os_type import OsType
 from hardware_component import HardwareComponent
 from gpu import Gpu
 
@@ -7,5 +8,5 @@ class GpuComponentFactory(HardwareComponentFactory):
     def __init__(self):
         super().__init__()
     
-    def create_component(self) -> HardwareComponent:
-        return Gpu()
+    def create_component(self, operating_system: OsType) -> HardwareComponent:
+        return Gpu(operating_system)
