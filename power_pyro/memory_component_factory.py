cd08c2c071ea162a2ec9436f6f13124e86e76a50
Ryann <ryann.arruda_159@hotmail.com>
2025-03-20 17:43:04 -0300

    Addition of operating_system parameter

--- power_pyro/memory_component_factory.py
+++ power_pyro/memory_component_factory.py
@@ -1,4 +1,5 @@
 from hardware_component_factory import HardwareComponentFactory
+from os_type import OsType
 from hardware_component import HardwareComponent
 from memory import Memory
 
@@ -7,5 +8,5 @@ class MemoryComponentFactory(HardwareComponentFactory):
     def __init__(self):
         super().__init__()
 
-    def create_component(self) -> HardwareComponent:
-        return Memory()
+    def create_component(self, operating_system: OsType) -> HardwareComponent:
+        return Memory(operating_system)
