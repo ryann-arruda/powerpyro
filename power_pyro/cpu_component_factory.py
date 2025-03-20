880d7f414704abc41859d47f166146039f1a8977
Ryann <ryann.arruda_159@hotmail.com>
2025-03-20 17:37:47 -0300

    Addition of operating_system parameter

--- power_pyro/cpu_component_factory.py
+++ power_pyro/cpu_component_factory.py
@@ -1,4 +1,5 @@
 from hardware_component_factory import HardwareComponentFactory
+from os_type import OsType
 from hardware_component import HardwareComponent
 from cpu import Cpu
 
@@ -7,5 +8,5 @@ class CpuComponentFactory(HardwareComponentFactory):
     def __init__(self):
         super().__init__()
 
-    def create_component(self) -> HardwareComponent:
-        return Cpu()
+    def create_component(self, operating_system: OsType) -> HardwareComponent:
+        return Cpu(operating_system)
