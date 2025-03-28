50f5934102a0363dc05a95fe73ee8c502402b089
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2025-03-28 16:17:23 -0300

    Add files via upload

--- power_pyro/hardware_component.py
+++ power_pyro/hardware_component.py
@@ -2,19 +2,45 @@ from abc import ABC, abstractmethod
 from os_type import OsType
 
 class HardwareComponent(ABC):
+    """Abstract base class representing a hardware component.
+
+    Attributes:
+        __total_energy_consumed (float): Total energy consumed by the hardware component.
+        __operating_system (OsType): The operating system where the hardware component is running.
+    """
     def __init__(self, operating_system: OsType):
-        self.__total_energy_consumed:float = 0.0
+        self.__total_energy_consumed: float = 0.0
         self.__operating_system: OsType = operating_system
 
     def get_total_energy_consumed(self) -> float:
+        """Retrieves the total energy consumed by the hardware component.
+
+        Returns:
+            float: Total energy consumed.
+        """
         return self.__total_energy_consumed
 
     def get_operating_system(self) -> OsType:
+        """Retrieves the operating system of the hardware component.
+
+        Returns:
+            OsType: The operating system.
+        """
         return self.__operating_system
     
     def update_energy_consumed(self, energy_consumed_per_time: float) -> None:
+        """Updates the total energy consumed by adding the energy consumed over a period of time.
+
+        Args:
+            energy_consumed_per_time (float): The energy consumed during a specific time interval.
+        """
         self.__total_energy_consumed += energy_consumed_per_time
 
     @abstractmethod
     def get_power(self) -> float:
+        """Abstract method to retrieve the power consumption of the hardware component.
+
+        Returns:
+            float: Power consumption in watts.
+        """
         pass
