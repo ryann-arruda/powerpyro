bf6f1b0fc85f0f4c1a3803b9912f036df0c952fc
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2025-04-01 14:46:43 -0300

    Add files via upload

--- power_pyro/processing_unit.py
+++ power_pyro/processing_unit.py
@@ -12,28 +12,52 @@ if os.name == 'nt':
     from LibreHardwareMonitor.Hardware import Computer
 
 class ProcessingUnit(HardwareComponent):
-
+    """Abstract base class representing a processing unit.
+    
+    This class extends `HardwareComponent` and provides common functionality
+    for processing units, including name management and computer interaction.
+    """
     def __init__(self, operating_system: OsType):
         super().__init__(operating_system)
 
-        self.__name:str = None
+        self.__name: str = None
 
         if operating_system == OsType.WINDOWS:
             self.__computer: Computer = Computer()
 
+    @property
     def get_name(self) -> str:
+        """Gets the name of the processing unit.
+
+        Returns:
+            str: The name of the processing unit.
+        """
         return self.__name
     
-    def set_name(self, name:str) -> None:
+    @get_name.setter
+    def set_name(self, name: str) -> None:
+        """Sets the name of the processing unit.
+        
+        Args:
+            name (str): The new name of the processing unit.
+        """
         self.__name = name
     
+    @property
     def get_computer(self) -> Computer:
+        """Gets the computer instance associated with the processing unit.
+
+        Returns:
+            Computer: The computer instance.
+        """
         return self.__computer
     
     def open(self) -> None:
+        """Opens the computer monitoring instance."""
         self.__computer.Open()
     
     def close(self) -> None:
+        """Closes the computer monitoring instance."""
         self.__computer.Close()
     
     @abstractmethod
