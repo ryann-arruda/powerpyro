00de1c218b1d704d186a57a24f4bcbc6e1b645e9
Alexandre Bezerra de Lima <53580956+Alexandreprog@users.noreply.github.com>
2025-04-01 16:49:34 -0300

    Add files via upload

--- power_pyro/identify_hardware_manufacturer_exception.py
+++ power_pyro/identify_hardware_manufacturer_exception.py
@@ -1,6 +1,11 @@
 from hardware_type import HardwareType
 
 class IdentifyHardwareManufacturerException(Exception):
+    """Exception raised when the hardware manufacturer cannot be identified.
 
+    Args:
+        hardware_type (HardwareType): The type of the hardware whose manufacturer could not be identified.
+        msg (str): Descriptive error message (optional, default is "Unable to identify hardware manufacturer").
+    """
     def __init__(self, hardware_type: HardwareType, msg: str = "Unable to identify hardware manufacturer"):
         super().__init__(hardware_type.name + ": " + msg)
--- power_pyro/invalid_keys_error_exception.py
+++ power_pyro/invalid_keys_error_exception.py
@@ -1,6 +1,10 @@
 from typing import Dict
 
 class InvalidKeysErrorException(Exception):
-    
+    """Exception raised when the provided keys do not match the expected ones.
+
+    Args:
+        msg (str): Descriptive error message (optional). Default is "The keys provided do not match those expected".
+    """
     def __init__(self, msg: str = "The keys provided do not match those expected"):
         super().__init__(msg)
