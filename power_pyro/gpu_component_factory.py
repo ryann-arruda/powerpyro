51fedcde96922882d08c2cb159a603a80c65a9d1
Ryann <ryann.arruda_159@hotmail.com>
2025-04-02 17:04:00 -0300

    Adding exception handling in the create_component method

--- power_pyro/gpu_component_factory.py
+++ power_pyro/gpu_component_factory.py
@@ -2,6 +2,8 @@ from hardware_component_factory import HardwareComponentFactory
 from os_type import OsType
 from hardware_component import HardwareComponent
 from gpu import Gpu
+from identify_hardware_manufacturer_exception import IdentifyHardwareManufacturerException
+from object_creation_exception import ObjectCreationException
 
 class GpuComponentFactory(HardwareComponentFactory):
     """Factory class for creating GPU components.
@@ -13,4 +15,7 @@ class GpuComponentFactory(HardwareComponentFactory):
         super().__init__()
     
     def create_component(self, operating_system: OsType) -> HardwareComponent:
-        return Gpu(operating_system)
+        try:
+            return Gpu(operating_system)
+        except IdentifyHardwareManufacturerException as e:
+            raise ObjectCreationException(additional_info = str(e))
